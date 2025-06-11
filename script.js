0b403b498dc7495c2493c775b5fe5adba5ba1d47
fridavatnarlie <frida.vatnar.lie@icloud.com>
2025-06-11 16:19:12 +0200

    prøve å fikse timer, ble ikke helt ferdig. fortsetter i morgen

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const klikklyd = document.getElementById("klikklyd")
 
 let antallKlikk = 0
 let spillStartet = false
+let klarForSteking = false
 
 function startSpill() {
         if (!spillStartet) {
@@ -38,7 +39,42 @@ ingredienser.forEach(function(ingrediens){
             fullbolle.style.right = "515px"
             fullbolle.style.height = "230px"
 
+            klarForSteking = true
+
         }
     })
 })
+
+fullbolle.addEventListener("click", function() {
+    if (klarForSteking) {
+        fullbolle.style.display = "none"
+        startTimer(10)
+        klarForSteking = false
+    }
+});
+
+function visKake() {
+    kake.style.display = "block"
+    kake.style.position = "absolute"
+    kake.style.bottom = "30px"
+    kake.style.right = "500px"
+    kake.style.height = "230px"
+}
+
+function startTimer(sekunder) {
+    let tidIgjen = sekunder
+    timerElm.textContent = tidIgjen
+    timerElm.style.display = "block"
+
+    const nedtelling = setInterval(() => {
+        tidIgjen--
+        timerElm.textContent = tidIgjen
+
+        if (tidIgjen <= 0) {
+            clearInterval(nedtelling)
+            timerElm.style.display = "none"
+            visKake()
+        }
+    }, 1000);
+}
 
